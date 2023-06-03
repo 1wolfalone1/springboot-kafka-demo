5c43bce4b7eb9283aa6eb2a9b181c51863754e2b
NNhatTThien <thientryhard@gmail.com>
2023-06-03 17:07:44 +0700

    Demo kafka

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+
+        return args -> {
+            for (int i = 0; i < 100; i++) {
+                kafkaTemplate.send("wolfalone", "hello kafka ahihi" + i);
+            }
+        };
+
+    }
+}
